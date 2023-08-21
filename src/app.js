91d079d160f44764e8167dfda23050903b48e2ad
Jkoury997 <jorge_koury@me.com>
2023-08-21 17:55:40 -0300

    Terminado

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 3001;
+const port = process.env.PORT || 3001;
 
 // Declarar una carpeta statica
 app.use(express.static(path.join(__dirname, "../public")));
 
 // Levantar un servidor 
-app.listen(PORT, () =>
-    console.log("Servidor Funcionando en", PORT)
+app.listen(port, () =>
+    console.log("Servidor Funcionando en", port)
 );
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
 })
